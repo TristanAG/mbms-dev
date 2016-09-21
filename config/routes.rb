d0ddf9dca9559534fe144b06016d97e8d5a3ab1a
Tristan Gruener <tristan.gruener@gmail.com>
2016-09-21 13:43:36 -0700

    made static_pages controller and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/teachers', to: 'static_pages#teachers'
+
+  get '/schedule', to: 'static_pages#schedule'
+
+  get '/classes', to: 'static_pages#classes'
+
+  get '/contact', to: 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
