66cb47dd2360cb52a7ec432b37d08f420217d5a2
Tristan Gruener <tristan.gruener@gmail.com>
2016-09-21 16:01:07 -0700

    still working on the route stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/teachers', to: 'static_pages#teachers'
   get '/schedule', to: 'static_pages#schedule'
   get '/classes', to: 'class_pages#index'
+  #get '/class_pages/show', to: 'class_pages#show', :as => :show
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
