f540d56cb91ecf51fd4b34554f24c62773e64da3
Tristan Gruener <tristan.gruener@gmail.com>
2016-09-21 14:18:23 -0700

    initial work on stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   resources :class_pages
+
   root 'static_pages#home'
-
   get '/teachers', to: 'static_pages#teachers'
-
   get '/schedule', to: 'static_pages#schedule'
-
   get '/classes', to: 'class_pages#index'
-
   get '/contact', to: 'static_pages#contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
